0dc2bf06585f3f74c8f79ef678bed7026331fb80
bhuvan <69458259+bhuvanbhoge@users.noreply.github.com>
2024-03-02 00:31:19 +0530

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TranformationsPage = () => {
+  return (
+    <div>TranformationsPage</div>
+  )
+}
+
+export default TranformationsPage
+
+// localhost:3000/transformations       -    NOT
+// localhost:3000/transformations/ [id] -    Ok
